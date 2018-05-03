ead855974898fac5fa8accf131c14a521889f3de
emoonight <emoonight@qq.com>
2018-05-03 21:05:00 +0800

    ecs

--- src/game/test/ESC/EcsEntity.ts
+++ src/game/test/ESC/EcsEntity.ts
@@ -1,4 +1,10 @@
 module ecs
 {
-    
+    export class EcsEntity
+    {
+        public IsReserved:boolean;
+        public Mask:EcsComponentMask=new EcsComponentMask();
+        public ComponentsCount:number;
+        public Components:ComponentsLink[] = new Array<ComponentsLink>(8);  
+    }
 }
