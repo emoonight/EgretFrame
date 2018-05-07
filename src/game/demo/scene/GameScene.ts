770cfbfc6501a6f67699efdf58779a719598a4ae
emoonight <emoonight@qq.com>
2018-05-07 20:50:49 +0800

    随机数组

--- src/game/demo/scene/GameScene.ts
+++ src/game/demo/scene/GameScene.ts
@@ -9,7 +9,7 @@ class GameScene extends Scene
 
         for(let i = 0 ; i < 30 ; i++)
         {
-            let num = MathUtils.RandomRange(0,3);
+            let num = MathUtils.Random(0,3);
             let node = new GridNode(i,num,this.getLayer(LayerType.Sc_Entity));
             this.m_grids.push(node);
         }
--- src/game/utils/MathUtils.ts
+++ src/game/utils/MathUtils.ts
@@ -5,8 +5,32 @@ class MathUtils
      * @param min  最小值
      * @param max  最大值
      */
-    public static RandomRange(min:number,max:number):number
+    public static Random(min:number,max:number):number
     {
        return Math.floor(Math.random()*(max-min+1)+min); 
+    }
+
+    /**
+     * 在start 到 end 之间取值生成len 个数
+     * @param len 个数
+     * @param start 起始值
+     * @param end 结束值
+     */
+    public static RandomRange(len:number,start:number,end:number):number[]
+    {
+        let arr:number[] =[];
+        let inner = (start,end)=>{
+            let span = end - start;
+            return Math.floor(Math.random() * span + start);
+        }
+
+        while(arr.length < len)
+        {
+            let num = inner(start,end)
+            if(arr.indexOf(num) == -1)
+                arr.push(num);
+        }
+
+        return arr;
     }
 }
