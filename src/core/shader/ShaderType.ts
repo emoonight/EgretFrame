05c165458ffa02408b2bfc5365d5d17ea38d2983
emoonight <emoonight@qq.com>
2018-09-14 21:13:21 +0800

    webgl

---
+++ src/core/shader/ShaderType.ts
@@ -0,0 +1,12 @@
+enum ShaderType 
+{
+    /**
+     * 顶点着色
+     */
+    VERTEX,
+    
+    /**
+     * 片段着色/pixel
+     */
+    FRAGEMENT
+}
--- src/game/demo/scene/WGLScene.ts
+++ src/game/demo/scene/WGLScene.ts
@@ -1,13 +1,45 @@
 class WGLScene extends Scene
 {
-    private m_shaderProgram:WebGLProgram
+
+    private m_modelViewMatrix;
+    private m_projectionMatrix;
+
+    private m_shaderProgram:WebGLProgram;
+
+    private m_vertexSource = 'attribute vec3 vertexPos;\n'+
+                            'attribute vec4 vertexColor;\n'+
+                            'uniform mat4 modelViewMatrix;\n'+
+                            'uniform mat4 projectionMatrix;\n'+
+                            'varying vec4 vColor;\n'+
+                            'void main(void){\n'+
+                            //返回经过变换和投影的顶点值
+                            ' gl_Position = projectionMatrix * modelViewMatrix * vec4(vertexPos,1.0);\n'+
+                            ' vColor = vertextColor;\n'+
+                            '}\n';
+    
+
+    private m_fragmentSource = 'precision mediump float;\n'+
+                                'varying vec4 vColor;\n'+
+                                'void main(void){\n'+
+                                //始终输出白色
+                                ' gl_fragColor = vColor;\n'+
+                                '}\n';
 
     
+    private m_shaderVertexPositionAttr:number;
+    private m_shaderProjectionMatrixUniform:WebGLUniformLocation;
+    private m_shaderModelViewMatrixUniform:WebGLUniformLocation;
+    
+
     public onEnter(id:number)
     {
         super.onEnter(id);
 
+        let gl = this.getGL();
+        this.buildShaderProgram(gl);
 
+        let cube = this.getCube(gl);
+        this.drawCube(gl,cube);
     }
 
     public getGL():WebGLRenderingContext
@@ -20,22 +52,60 @@ class WGLScene extends Scene
             gl = canvas.getContext('experimental-webgl')
             
             if(gl)
-                return gl ;
+                return gl;
         }
 
         return null;
     }
 
 
-    private buildShader()
+    private initMatrix(canvas:HTMLCanvasElement)
     {
-
+        
     }
 
 
-    private createShader()
+    private buildShaderProgram(gl:WebGLRenderingContext)
     {
-        
+        let vertexShader = this.createShader(gl,gl.VERTEX_SHADER,this.m_vertexSource);
+        let fragmentShader = this.createShader(gl,gl.FRAGMENT_SHADER,this.m_fragmentSource);
+
+        this.m_shaderProgram = gl.createProgram();
+        gl.attachShader(this.m_shaderProgram,vertexShader);
+        gl.attachShader(this.m_shaderProgram,fragmentShader);
+
+        this.m_shaderVertexPositionAttr = gl.getAttribLocation(this.m_shaderProgram,'vertexPos');
+        gl.enableVertexAttribArray(this.m_shaderVertexPositionAttr);
+
+        this.m_shaderProjectionMatrixUniform = gl.getUniformLocation(this.m_shaderProgram,'projectionMatrix');
+        this.m_shaderModelViewMatrixUniform = gl.getUniformLocation(this.m_shaderProgram,'modelViewMatrix');
+
+        if(!gl.getProgramParameter(this.m_shaderProgram,gl.LINK_STATUS))
+            console.log('shaderProgram init failed');
+    }
+
+    /**
+     * type 
+     */
+    private createShader(gl:WebGLRenderingContext,type:number,source:string):WebGLShader
+    {
+        if(type != gl.VERTEX_SHADER && type !=gl.FRAGMENT_SHADER )
+        {
+            console.log('Error shader type');
+            return void 0;
+        }
+
+        let shader = gl.createShader(type);
+        gl.shaderSource(shader,source);
+        gl.compileShader(shader);
+
+        if(!gl.getShaderParameter(shader,gl.COMPILE_STATUS))
+        {
+            console.log(gl.getShaderInfoLog(shader));
+            return void 0;
+        }
+
+        return shader;
     }
 
     private getCube(gl:WebGLRenderingContext)
@@ -132,29 +202,18 @@ class WGLScene extends Scene
 
     private drawCube(gl:WebGLRenderingContext,obj)
     {   
-        //顶点着色glsl 源码
-        const vertexShaderStr = 'attribute vec3 vertexPos;\n'+
-                                'attribute vec4 vertexColor;\n'+
-                                'uniform mat4 modelViewMatrix;\n'+
-                                'uniform mat4 projectionMatrix;\n'+
-                                'varying vec4 vColor;\n'+
-                                'void main(void){\n'+
-                                //返回经过变换和投影的顶点值
-                                ' gl_Position = projectionMatrix * modelViewMatrix * vec4(vertexPos,1.0);\n'+
-                                ' vColor = vertextColor;\n'+
-                                '}\n';
-                            
-        //片段着色glsl 源码
-        const fragementShaderStr = 'precision mediump float;\n'+
-                                    'varying vec4 vColor;\n'+
-                                    'void main(void){\n'+
-                                    //始终输出白色
-                                    ' gl_fragColor = vColor;\n'+
-                                    '}\n';
-
-
         gl.clearColor(0.0,0.0,0.0,1.0); //黑色背景
         gl.enable(gl.DEPTH_TEST);   //深度测试
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        gl.useProgram(this.m_shaderProgram);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER,obj.buffer);
+        gl.vertexAttribPointer(this.m_shaderVertexPositionAttr,obj.vertSize,gl.FLOAT,false,0,0);
+        gl.bindBuffer(gl.ARRAY_BUFFER,obj.colorBuffer);
+        gl.vertexAttribPointer(this.m_shaderVertexPositionAttr,obj.colorSize,gl.FLOAT,false,0,0);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER,obj.indices);
+
     }
 }
